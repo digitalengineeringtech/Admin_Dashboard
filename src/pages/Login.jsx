d6e999bf1ff6e94586092333427ac4775cc5859c
det-frontend <digitalengineeringtech.frontend@gmail.com>
2024-02-07 12:05:15 +0630

    gg

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center h-screen items-center login_bg">
+      <div className="flex flex-col items-center">
+        <div className="">logo</div>
+        <div className="flex">
+          <div className="">left</div>
+          <div className="">right</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FAFAFA",
+        secondary: "#FFFFFF",
+        detail: "#45B54B",
+        text: "#3C3C3C",
+        icon: "#7C7C7C",
+        input: "#EEEEEE",
+        subText: "#DDDDDD",
+        danger: "#EB6A6A",
+      },
+    },
   },
   plugins: [],
 };

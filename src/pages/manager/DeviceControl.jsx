073b856a4deac7edd331d8916fcb62cb61423341
HtooKhant-vexix <vexixongaming02@gmail.com>
2024-05-07 11:26:46 +0630

    h

--- src/pages/manager/DeviceControl.jsx
+++ src/pages/manager/DeviceControl.jsx
@@ -29,7 +29,7 @@ const DeviceControl = () => {
   end = new Date(end);
 
   const { loading, setLoading } = useContext(LoadContext);
-  const client = mqtt.connect("wss://detpos:asdffdsa@192.168.0.100:9001");
+  // const client = mqtt.connect("wss://detpos:asdffdsa@192.168.0.100:9001");
   const { refresh, setRefresh } = useContext(Re);
 
   const [token, setToken] = useState("none");
@@ -83,11 +83,11 @@ const DeviceControl = () => {
 
   const nozzle1FuelDetailRef = useRef({ liter: "", price: "" });
 
-  client.on("connect", () => {
-    client.subscribe("detpos/#", (err) => {
-      err && console.log(err);
-    });
-  });
+  // client.on("connect", () => {
+  //   client.subscribe("detpos/#", (err) => {
+  //     err && console.log(err);
+  //   });
+  // });
 
   const [permitData, setPermitData] = useState([]);
   const [approveData, setApproveData] = useState([]);
@@ -329,144 +329,144 @@ const DeviceControl = () => {
     hour = 12;
   }
 
-  client.on("message", (topic, message) => {
-    if (topic.startsWith("detpos/device/permit/") && /[1-8]$/.test(topic)) {
-      const prefix = message.toString().substring(0, 2); // "01"
+  // client.on("message", (topic, message) => {
+  //   if (topic.startsWith("detpos/device/permit/") && /[1-8]$/.test(topic)) {
+  //     const prefix = message.toString().substring(0, 2); // "01"
 
-      const topicCount = payloadHistoryRef.current.filter(
-        (t) => t === parseInt(prefix)
-      ).length;
-      if (topicCount < 2) {
-        setPayloadHistory((prevTopics) => [...prevTopics, parseInt(prefix)]);
-      }
-      // updatePermitData(prefix);
+  //     const topicCount = payloadHistoryRef.current.filter(
+  //       (t) => t === parseInt(prefix)
+  //     ).length;
+  //     if (topicCount < 2) {
+  //       setPayloadHistory((prevTopics) => [...prevTopics, parseInt(prefix)]);
+  //     }
+  //     // updatePermitData(prefix);
 
-      setNoMorePermit("hhh");
-    }
+  //     setNoMorePermit("hhh");
+  //   }
 
-    if (topic === "detpos/local_server") {
-      const prefix = message.toString().substring(0, 2); // "01"
-      setAllDone(prefix);
-      // setFetchNew((prev)=>!prev)
-    }
+  //   if (topic === "detpos/local_server") {
+  //     const prefix = message.toString().substring(0, 2); // "01"
+  //     setAllDone(prefix);
+  //     // setFetchNew((prev)=>!prev)
+  //   }
 
-    //  add new
-    if (topic === "detpos/local_server/preset") {
-      const prefix = message.toString().substring(0, 2); // "01"
-      // setAllDone(prefix);
-      setReadyDespenserHistory((prevTopics) => [
-        ...prevTopics,
-        parseInt(prefix),
-      ]);
-      console.log("hell");
-      // setFetchNew((prev)=>!prev)
-    }
-    //add new
+  //   //  add new
+  //   if (topic === "detpos/local_server/preset") {
+  //     const prefix = message.toString().substring(0, 2); // "01"
+  //     // setAllDone(prefix);
+  //     setReadyDespenserHistory((prevTopics) => [
+  //       ...prevTopics,
+  //       parseInt(prefix),
+  //     ]);
+  //     console.log("hell");
+  //     // setFetchNew((prev)=>!prev)
+  //   }
+  //   //add new
 
-    if (topic === "detpos/device/price") {
-      // setPriceChange(true);
-    }
+  //   if (topic === "detpos/device/price") {
+  //     // setPriceChange(true);
+  //   }
 
-    if (topic === "detpos/local_server/mode") {
-      if (message.toString() === "allow") {
-        setPermitd(true);
-      } else if (message.toString() === "manual") {
-        setPermitd(false);
-      }
-    }
+  //   if (topic === "detpos/local_server/mode") {
+  //     if (message.toString() === "allow") {
+  //       setPermitd(true);
+  //     } else if (message.toString() === "manual") {
+  //       setPermitd(false);
+  //     }
+  //   }
 
-    if (topic === "detpos/local_server/price_change") {
-      // setPriceChange(true);
-    }
+  //   if (topic === "detpos/local_server/price_change") {
+  //     // setPriceChange(true);
+  //   }
 
-    if (topic.startsWith("detpos/local_server") && /[1-8]$/.test(topic)) {
-      const prefix = message.toString().substring(0, 2); // "01"
+  //   if (topic.startsWith("detpos/local_server") && /[1-8]$/.test(topic)) {
+  //     const prefix = message.toString().substring(0, 2); // "01"
 
-      const topicCount = readyDespenserHistoryRef.current.filter(
-        (t) => t === parseInt(prefix)
-      ).length;
-      if (topicCount < 1) {
-        setReadyDespenserHistory((prevTopics) => [
-          ...prevTopics,
-          parseInt(prefix),
-        ]);
-        // setApproveData((prevData) => [...prevData, parseInt(prefix)]);
-        // setPermitData((prevData) =>
-        //   prevData.filter((item) => item !== parseInt(prefix))
-        // );
-      }
-    }
+  //     const topicCount = readyDespenserHistoryRef.current.filter(
+  //       (t) => t === parseInt(prefix)
+  //     ).length;
+  //     if (topicCount < 1) {
+  //       setReadyDespenserHistory((prevTopics) => [
+  //         ...prevTopics,
+  //         parseInt(prefix),
+  //       ]);
+  //       // setApproveData((prevData) => [...prevData, parseInt(prefix)]);
+  //       // setPermitData((prevData) =>
+  //       //   prevData.filter((item) => item !== parseInt(prefix))
+  //       // );
+  //     }
+  //   }
 
-    // console.log(
-    //   approveData,
-    //   "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    // );
+  //   // console.log(
+  //   //   approveData,
+  //   //   "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  //   // );
 
-    // console.log(message.toString())
+  //   // console.log(message.toString())
 
-    // console.log(message.toString(), topic);
+  //   // console.log(message.toString(), topic);
 
-    if (topic === "detpos/device/whreq") {
-      const topicCount = liveDespenserHistoryRef.current.filter(
-        (t) => t == message.toString()
-      ).length;
-      if (topicCount < 2) {
-        setLiveDespenserHistory((prevTopics) => [
-          ...prevTopics,
-          message.toString(),
-        ]);
-      }
-    }
+  //   if (topic === "detpos/device/whreq") {
+  //     const topicCount = liveDespenserHistoryRef.current.filter(
+  //       (t) => t == message.toString()
+  //     ).length;
+  //     if (topicCount < 2) {
+  //       setLiveDespenserHistory((prevTopics) => [
+  //         ...prevTopics,
+  //         message.toString(),
+  //       ]);
+  //     }
+  //   }
 
-    if (topic === "detpos/device/req") {
-      const prefix = message.toString().substring(0, 2); // "01"
-      const suffix = message.toString().substring(2); // "cancel"
-      setPermitData((prevData) =>
-        prevData.filter((item) => item !== parseInt(prefix))
-      );
-      setApproveData((prevData) =>
-        prevData.filter((item) => item !== parseInt(prefix))
-      );
+  //   if (topic === "detpos/device/req") {
+  //     const prefix = message.toString().substring(0, 2); // "01"
+  //     const suffix = message.toString().substring(2); // "cancel"
+  //     setPermitData((prevData) =>
+  //       prevData.filter((item) => item !== parseInt(prefix))
+  //     );
+  //     setApproveData((prevData) =>
+  //       prevData.filter((item) => item !== parseInt(prefix))
+  //     );
 
-      setNoMorePermit(prefix);
-    }
+  //     setNoMorePermit(prefix);
+  //   }
 
-    if (topic.startsWith("detpos/device/Final/") && /[1-8]$/.test(topic)) {
-      let data = message.toString().split(regex);
-      setFinalData(data[0]);
-      setReFresh(!reFresh);
-      // setApproveData((prevData) =>
-      //   prevData.filter((item) => item !== parseInt(prefix))
-      // );
-      // setPer((prevData) =>
-      //   prevData.filter((item) => item !== parseInt(prefix))
-      // );
-    }
+  //   if (topic.startsWith("detpos/device/Final/") && /[1-8]$/.test(topic)) {
+  //     let data = message.toString().split(regex);
+  //     setFinalData(data[0]);
+  //     setReFresh(!reFresh);
+  //     // setApproveData((prevData) =>
+  //     //   prevData.filter((item) => item !== parseInt(prefix))
+  //     // );
+  //     // setPer((prevData) =>
+  //     //   prevData.filter((item) => item !== parseInt(prefix))
+  //     // );
+  //   }
 
-    // if (topic.startsWith("detpos/device/livedata/") && /[1-8]$/.test(topic)) {
-    //   let data = message.toString().split(regex);
-    //   const updatedNozzle1FuelDetail = {
-    //     liter: data[1],
-    //     price: data[2],
-    //     nozzleNo: data[0],
-    //   };
+  //   // if (topic.startsWith("detpos/device/livedata/") && /[1-8]$/.test(topic)) {
+  //   //   let data = message.toString().split(regex);
+  //   //   const updatedNozzle1FuelDetail = {
+  //   //     liter: data[1],
+  //   //     price: data[2],
+  //   //     nozzleNo: data[0],
+  //   //   };
 
-    //   const checkLive = {
-    //     nozzleNo: data[0],
-    //   };
+  //   //   const checkLive = {
+  //   //     nozzleNo: data[0],
+  //   //   };
 
-    //   checkLiveRef.current = {
-    //     ...checkLiveRef.current,
-    //     ...checkLive,
-    //   };
+  //   //   checkLiveRef.current = {
+  //   //     ...checkLiveRef.current,
+  //   //     ...checkLive,
+  //   //   };
 
-    //   nozzle1FuelDetailRef.current = {
-    //     ...nozzle1FuelDetailRef.current,
-    //     ...updatedNozzle1FuelDetail,
-    //   };
-    // }
-    // client.end();
-  });
+  //   //   nozzle1FuelDetailRef.current = {
+  //   //     ...nozzle1FuelDetailRef.current,
+  //   //     ...updatedNozzle1FuelDetail,
+  //   //   };
+  //   // }
+  //   // client.end();
+  // });
 
   // console.log(data_g_2.map((e) => parseInt(e.nozzle_no)));
   // console.log(permitData, "permit data");
@@ -475,7 +475,7 @@ const DeviceControl = () => {
       {loading && <LoaderCom />}
       <div className="flex items-start mt-[110px] h-full">
         <div className="w-full flex gap-5 flex-wrap items-center justify-center">
-          {data_g?.map((obj, index) => (
+          {/* {data_g?.map((obj, index) => (
             <Card
               disableButton={disableButton}
               setDisableButton={setDisableButton}
@@ -515,7 +515,7 @@ const DeviceControl = () => {
               dis={obj.dep_no}
               num={obj.nozzle_no}
             />
-          ))}
+          ))} */}
         </div>
         <div className="w-[50%] hidden">
           {/* <PrinterT ref={componentRef} pData={pData} /> */}
@@ -637,11 +637,11 @@ const DeviceControl = () => {
               }}
             >
               <h4>Thank you. Please come again.</h4>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
             </div>
           </div>
         </div>
